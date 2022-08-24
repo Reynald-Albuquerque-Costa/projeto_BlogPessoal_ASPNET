cb3a8bf3dfdebc1ed17570a9d69f5f021065eb62
Reynald Albuquerque <reynaldalbuquerquecosta@gmail.com>
2022-08-23 22:18:00 -0300

    @Reynald-Albuquerque-Costa[Task: Projeto incluído]{23/09/2022 22:17}

--- BlogAPI/Src/Modelos/UsuarioModelo.cs
+++ BlogAPI/Src/Modelos/UsuarioModelo.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using BlogAPI.Src.Utilidades;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
@@ -26,12 +27,24 @@ namespace BlogAPI.Src.Modelos
         public string Email { get; set; }
         public string Senha { get; set; }
         public string Foto { get; set; }
+        
+        [Required]
+        public TipoUsuario Tipo { get; set; }
+
 
         [JsonIgnore, InverseProperty("Criador")]
         public List<Postagem> MinhasPostagens { get; set; }
 
 
         #endregion
+    }
+
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum TipoUsuario
+    {
+        NORMAL,  
+        ADMINISTRADOR
+
     }
 
 }
--- BlogAPI/Startup.cs
+++ BlogAPI/Startup.cs
@@ -1,4 +1,9 @@
 using BlogAPI.Src.Contextos;
+using BlogAPI.Src.Repositorios;
+using BlogAPI.Src.Repositorios.Implementacoes;
+using BlogAPI.Src.Servicos;
+using BlogAPI.Src.Servicos.Iplementacoes;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -7,9 +12,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace BlogAPI
@@ -29,8 +36,38 @@ namespace BlogAPI
             // Configuração de Banco de dados
             services.AddDbContext<BlogPessoalContexto>(opt =>opt.UseSqlServer(Configuration["ConnectionStringsDev:DefaultConnection"]));
 
+            // Repositorios
+            services.AddScoped<IUsuario, UsuarioRepositorio>();
+            services.AddScoped<ITema, TemaRepositorio>();
+            services.AddScoped<IPostagem, PostagemRepositorio>();
+
+
             // Controladores
+            services.AddCors();
             services.AddControllers();
+
+            // Configuração de Serviços
+            services.AddScoped<IAutenticacao, AutenticacaoServicos>();
+
+            // Configuração do Token Autenticação JWTBearer
+            var chave = Encoding.ASCII.GetBytes(Configuration["Settings:Secret"]);
+            services.AddAuthentication(a =>
+            {
+                a.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                a.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(b =>
+            {
+                b.RequireHttpsMetadata = false;
+                b.SaveToken = true;
+                b.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(chave),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,9 +81,18 @@ namespace BlogAPI
             }
 
             // Ambiente de produção
-            contexto.Database.EnsureCreated();
+
+            // Rotas
             app.UseRouting();
 
+            app.UseCors(c => c
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+            );
+
+            // Autenticação e Autorização
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
